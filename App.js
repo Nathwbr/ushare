e492802d1fdfb151ee6990515bda0e274cedf2d5
Nathan WEBER <nweber@juniorisep.com>
2021-05-05 17:49:52 +0200

    /

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
-import AppNavigator from "./app/navigation/AppNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
-import AccountNavigator from "./app/navigation/AccountNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
+import AppNavigator from "./app/navigation/AppNavigator";
 
 export default function App() {
   return (
     <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
+      <AuthNavigator />
     </NavigationContainer>
   );
 }
